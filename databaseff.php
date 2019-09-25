d34b2688abf2c58842528a493321daac183996bd
MuhammdQasim <48544374+MuhammdQasim@users.noreply.github.com>
2019-09-25 02:52:40 -0700

    Add files via upload

---
+++ databaseff.php
@@ -0,0 +1,41 @@
+<?php
+Class Connection {
+private  $server = "mysql:host=localhost;dbname=muhmd_qasim";
+private  $user = "root";
+private  $pass = "";
+private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
+protected $con;
+ 
+            public function openConnection()
+             {
+               try
+                 {
+          $this->con = new PDO($this->server, $this->user,$this->pass,$this->options);
+          return $this->con;
+                  }
+               catch (PDOException $e)
+                 {
+                     echo "There is some problem in connection: " . $e->getMessage();
+                 }
+             }
+public function closeConnection() {
+     $this->con = null;
+  }
+}
+
+try
+{
+    $database = new Connection();
+    $db = $database->openConnection();
+    $sql = "SELECT * FROM data " ;
+    foreach ($db->query($sql) as $row) {
+    echo " ID: ".$row['Id'] . "<br>";
+    echo " Name: ".$row['Name'] . "<br>";
+    }
+}
+catch (PDOException $e)
+{
+    echo "There is some problem in connection: " . $e->getMessage();
+}
+
+?>
